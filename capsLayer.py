Mid-way fixing gradients

capsLayer.py
@@ -74,8 +74,8 @@ class CapsLayer(object):
                 self.unsquashed_capsules = tf.reshape(capsules, (cfg.batch_size, -1, self.vec_len, 1))
 
                 # [batch_size, 1152, 8, 1]
+                self.unsquashed_capsules = fix(self.unsquashed_capsules)
                 self.capsules = squash(self.unsquashed_capsules)
-                self.capsules= fix(self.capsules)
                 assert self.capsules.get_shape() == [cfg.batch_size, 1152, 8, 1]
                 return(self.capsules)
 
@@ -112,7 +112,9 @@ def routing(self, input, b_IJ):
     # W: [1, num_caps_i, num_caps_j * len_v_j, len_u_j, 1]
     self.W = tf.get_variable('Weight', shape=(1, 1152, 160, 8, 1), dtype=tf.float32,
                         initializer=tf.random_normal_initializer(stddev=cfg.stddev))
+    self.W = fix(self.W)
     self.biases = tf.get_variable('bias', shape=(1, 1, 10, 16, 1))
+    self.biases = fix(self.biases)
 
     # Eq.2, calc u_hat
     # Since tf.matmul is a time-consuming op,
@@ -125,7 +127,7 @@ def routing(self, input, b_IJ):
 
     self.u_hat = reduce_sum(self.W * input, axis=3, keepdims=True)
     self.u_hat = tf.reshape(self.u_hat, shape=[-1, 1152, 10, 16, 1])
-    self.u_hat = fix(self.u_hat)
+    #self.u_hat = fix(self.u_hat)
     assert self.u_hat.get_shape() == [cfg.batch_size, 1152, 10, 16, 1]
 
     # In forward, u_hat_stopped = u_hat; in backward, no gradient passed back from u_hat_stopped to u_hat
@@ -146,13 +148,13 @@ def routing(self, input, b_IJ):
                 self.s_J = tf.multiply(self.c_IJ, self.u_hat)
                 # then sum in the second dim, resulting in [batch_size, 1, 10, 16, 1]
                 self.s_J = reduce_sum(self.s_J, axis=1, keepdims=True) + self.biases
-                self.s_J = fix(self.s_J)
+                #self.s_J = fix(self.s_J)
                 assert self.s_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
 
                 # line 6:
                 # squash using Eq.1,
                 self.v_J = squash(self.s_J)
-                self.v_J = fix(self.v_J)
+                #self.v_J = fix(self.v_J)
                 assert self.v_J.get_shape() == [cfg.batch_size, 1, 10, 16, 1]
             elif r_iter < cfg.iter_routing - 1:  # Inner iterations, do not apply backpropagation
                 self.s_J = tf.multiply(self.c_IJ, u_hat_stopped)
@@ -165,7 +167,7 @@ def routing(self, input, b_IJ):
                 # batch_size dim, resulting in [1, 1152, 10, 1, 1]
                 self.v_J_tiled = tf.tile(self.v_J, [1, 1152, 1, 1, 1])
                 self.u_produce_v = reduce_sum(u_hat_stopped * self.v_J_tiled, axis=3, keepdims=True)
-                self.u_produce_v = fix(self.u_produce_v)
+                #self.u_produce_v = fix(self.u_produce_v)
                 assert self.u_produce_v.get_shape() == [cfg.batch_size, 1152, 10, 1, 1]
 
                 # b_IJ += tf.reduce_sum(self.u_produce_v, axis=0, keep_dims=True)