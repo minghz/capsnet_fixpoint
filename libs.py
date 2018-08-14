091fb5d660d740870b1c45566a36f908cba4af86
Minghua Zhao <minghz42@gmail.com>
2018-08-13 22:34:18 -0400

    Use different aliases for custom functions

--- libs.py
+++ libs.py
@@ -4,29 +4,29 @@ from tensorflow.python.framework import ops
 from config import cfg
 
 # Flooring resolution - custom OP
-rf = tf.load_op_library('./custom_ops/floor_resolution.so')
-floor_resolution = rf.floor_resolution
+fr = tf.load_op_library('./custom_ops/floor_resolution.so')
+floor_resolution = fr.floor_resolution
 @ops.RegisterGradient("FloorResolution")
 def _floor_resolution_grad(op, grad):
-    return rf.floor_resolution_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
+    return fr.floor_resolution_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
 
 
 # Nearest resolution - custom OP
-rf = tf.load_op_library('./custom_ops/nearest_resolution.so')
-nearest_resolution = rf.nearest_resolution
+nr = tf.load_op_library('./custom_ops/nearest_resolution.so')
+nearest_resolution = nr.nearest_resolution
 # Gradient registration for out custom operation
 @ops.RegisterGradient("NearestResolution")
 def _nearest_resolution_grad(op, grad):
-    return rf.nearest_resolution_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
+    return nr.nearest_resolution_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
 
 
 # Stochastic resolution - custom OP
-rf = tf.load_op_library('./custom_ops/stochastic_resolution.so')
-stochastic_resolution = rf.stochastic_resolution
+sr = tf.load_op_library('./custom_ops/stochastic_resolution.so')
+stochastic_resolution = sr.stochastic_resolution
 # Gradient registration for out custom operation
 @ops.RegisterGradient("StochasticResolution")
 def _stochastic_resolution_grad(op, grad):
-    return rf.stochastic_resolution_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
+    return sr.stochastic_resolution_grad(grad, op.inputs[0], op.inputs[1], op.inputs[2])
 
 
 def fix(x):
